d583c7b5e4b9b08ea9c56de5adc38da745a3829b
jidnyashakhapne <144833609+jidnyashakhapne@users.noreply.github.com>
2025-07-07 14:10:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Start Coding - Java/start-coding-java.java
@@ -0,0 +1,10 @@
+class Geeks {
+
+    // Function to print hello
+    static void printHello() {
+
+        // Your code here
+        //System.out.print("Hello World")
+        System.out.println("Hello World");
+    }
+}
